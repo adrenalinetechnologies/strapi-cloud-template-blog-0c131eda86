5963e723f507cd176f547ee4d1434f2f68a006ef
Maciej Chlebny <maciej.chlebny@adrentech.com>
2023-11-06 10:00:51 +0100

    kb-model

--- src/api/header/controllers/header.js
+++ src/api/header/controllers/header.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  author controller
+ * header controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::author.author');
+module.exports = createCoreController('api::header.header');
